dacf293d10b6be217e2cd5e9ac5190d9dc6cc4c4
Pavel Zhuravlev <propz@mail.ru>
2022-05-12 07:03:04 -0500

    update the task02-slice.go file

--- task02-slice.go
+++ task02-slice.go
@@ -2,16 +2,24 @@ package homework
 
 func Reverse(input []int64) (result []int64) {
 
-	i := len(input) - 1
+	if input != nil {
 
-	result = make([]int64, i+1)
-	copy(result, input)
-	j := 0
-	for i >= 0 {
-		result[j] = input[i]
-		i--
-		j++
+		i := len(input) - 1
+
+		result = make([]int64, i+1)
+		copy(result, input)
+		j := 0
+		for i >= 0 {
+			result[j] = input[i]
+			i--
+			j++
+		}
+
+		return result
+
+	} else {
+		var result []int64
+		return result
 	}
 
-	return result
 }
