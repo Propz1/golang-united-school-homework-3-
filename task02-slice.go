cb82910bff8e254b22442fbd57f721bdc7becfe2
Pavel Zhuravlev <propz@mail.ru>
2022-05-12 05:54:56 -0500

    change the task02-slice.go

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,17 @@
 package homework
 
-func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+func Reverse(input []int64) (result []int64) {
+
+	i := len(input) - 1
+
+	result = make([]int64, i+1)
+	copy(result, input)
+	j := 0
+	for i >= 0 {
+		result[j] = input[i]
+		i--
+		j++
+	}
+
+	return result
 }
