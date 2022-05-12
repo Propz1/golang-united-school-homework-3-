cf5da8ccb3e658b4a13fe6f0772eb29f55262f59
Pavel Zhuravlev <propz@mail.ru>
2022-05-12 02:34:00 -0500

    update the task01-arrays.go

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,12 +1,16 @@
 package homework
 
-func average(input [15]float32) (result float32) {
+func Average(input [15]float32) (result float32) {
 
 	var sum float32 = 0
+	n := 0
 
 	for _, i := range input {
 		sum = sum + i
+		if i != 0 {
+			n++
+		}
 	}
 
-	return sum / float32(len(input))
+	return sum / float32(n)
 }
