b5f0c165bded0f5b1a742b19e51cf66af95d7118
Pavel Zhuravlev <propz@mail.ru>
2022-05-13 10:17:58 -0500

    update the task02-slice_test.go file

--- task02-slice_test.go
+++ task02-slice_test.go
@@ -7,29 +7,29 @@ import (
 
 func TestReverse(t *testing.T) {
 
-	t.Run("Default_full", func(t *testing.T) {
+	//t.Run("Default_full", func(t *testing.T) {
 
-		var input = []int64{1, 2, 5, 15}
-		var result = []int64{15, 5, 2, 1}
+	var input = []int64{1, 2, 5, 15}
+	var result = []int64{15, 5, 2, 1}
 
-		realResult := Reverse(input)
+	realResult := Reverse(input)
 
-		if !reflect.DeepEqual(realResult, result) {
-			t.Errorf("Expected result %v != %v real result", result, realResult)
-		}
+	if !reflect.DeepEqual(realResult, result) {
+		t.Errorf("Expected result %v != %v real result", result, realResult)
+	}
 
-	})
+	//})
 
-	t.Run("Default_nil ", func(t *testing.T) {
+	// t.Run("Default_nil ", func(t *testing.T) {
 
-		var input []int64
-		var result = []int64(nil)
+	// 	var input []int64
+	// 	var result = []int64(nil)
 
-		realResult := Reverse(input)
+	// 	realResult := Reverse(input)
 
-		if !reflect.DeepEqual(realResult, result) {
-			t.Errorf("Expected result %v != %v real result", result, realResult)
-		}
+	// 	if !reflect.DeepEqual(realResult, result) {
+	// 		t.Errorf("Expected result %v != %v real result", result, realResult)
+	// 	}
 
-	})
+	// })
 }
