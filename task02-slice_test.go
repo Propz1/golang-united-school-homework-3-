57fc4c1682916e6275ca1f22aea04bcf035f9b22
Pavel Zhuravlev <propz@mail.ru>
2022-05-13 09:44:57 -0500

    delete the homework_test.go file

---
+++ task02-slice_test.go
@@ -0,0 +1,35 @@
+package homework
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReverse(t *testing.T) {
+
+	t.Run("Default_full", func(t *testing.T) {
+
+		var input = []int64{1, 2, 5, 15}
+		var result = []int64{15, 5, 2, 1}
+
+		realResult := Reverse(input)
+
+		if !reflect.DeepEqual(realResult, result) {
+			t.Errorf("Expected result %v != %v real result", result, realResult)
+		}
+
+	})
+
+	t.Run("Default_nil ", func(t *testing.T) {
+
+		var input []int64
+		var result = []int64(nil)
+
+		realResult := Reverse(input)
+
+		if !reflect.DeepEqual(realResult, result) {
+			t.Errorf("Expected result %v != %v real result", result, realResult)
+		}
+
+	})
+}
